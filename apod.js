5f7eb6b51af6a942fcf3f3ae5c882fd2423374ef
deediana <dianakihembo@gmail.com>
2022-08-05 21:18:05 -0500

    Astronomy

---
+++ apod.js
@@ -0,0 +1,50 @@
+$(document).ready(
+    function(){
+        $("#view_button").click(getData);
+});
+
+function getData(){
+    getPicture();
+    getTitle();;
+}
+
+function getPicture(){
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        type: "GET",
+        data: { api_key: "JHMqiQcxfQcbZNCFQbEM6WK1EcWDgo02Mw15pZFY",
+                date: $("#date").val()},
+        dataType: "json",
+        "success": showPicture,
+        "error": noPicture
+        });
+    };
+
+    function showPicture(data){
+        $("#pic").attr("src", data.url);
+    };
+
+    function noPicture(error){
+        alert(error.responseText);
+    };
+
+function getTitle(){
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        type: "GET",
+        data: { api_key: "JHMqiQcxfQcbZNCFQbEM6WK1EcWDgo02Mw15pZFY",
+                date: $("#date").val()},
+        dataType: "json",
+        "success": showTitle,
+        "error": noTitle
+            });
+        };
+
+    function showTitle(data){
+        document.getElementById("about").textContent = "";
+        $("#about").append("Title : "+ data.title);    
+    };
+
+    function noTitle(error){
+        alert(error.responseText);
+    };
